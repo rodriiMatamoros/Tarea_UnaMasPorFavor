134770d2a71908351f3cb377e0911eab985f59bd
Rodrigo Matamoros Becerra <rodri@MacBook-Pro-de-Rodrigo.local>
2023-03-26 15:59:18 +0200

    commit esPalindromoRecursivo

--- src/main/java/org/example/Ejercicio4/esPalindromoRecursivo.java
+++ src/main/java/org/example/Ejercicio4/esPalindromoRecursivo.java
@@ -1,2 +1,30 @@
 package org.example.Ejercicio4;
 
+//Implemente de forma recursiva una función en java que nos diga si una cadena de caracteres es simétrica (un palíndromo)
+// y preguntar por consola que frase quiere poner el usuario.
+
+import java.util.Scanner;
+
+public class esPalindromoRecursivo {
+    public static boolean esPalindromo(String cadena, int i, int j) {
+        if (i >= j) {
+            return true;
+        }
+        if (cadena.charAt(i) != cadena.charAt(j)) {
+            return false;
+        }
+        return esPalindromo(cadena, i + 1, j - 1);
+    }
+
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce una frase: ");
+        String cadena = scanner.nextLine();
+        scanner.close();
+        if (esPalindromo(cadena, 0, cadena.length() - 1)) {
+            System.out.println("Es palindromo");
+        } else {
+            System.out.println("No es palindromo");
+        }
+    }
+}

8855f4086c5e8265cbdcc3ea11666f165dcc0cbc
Rodrigo Matamoros Becerra <rodri@MacBook-Pro-de-Rodrigo.local>
2023-03-26 16:39:28 +0200

    commit EuclidesLambda

---
+++ src/main/java/org/example/Ejercicio5/EuclidesLambda.java
@@ -0,0 +1,25 @@
+package org.example.Ejercicio5;
+import java.util.Scanner;
+import java.util.function.BiFunction;
+
+public class EuclidesLambda {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introducir un valor: ");
+        int numero1 = scanner.nextInt();
+        System.out.print("Introducir un valor: ");
+        int numero2 = scanner.nextInt();
+        scanner.close();
+        BiFunction<Integer, Integer, Integer> mcd = (a, b) -> {
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        };
+        int resultado = mcd.apply(numero1, numero2);
+        System.out.println("El máximo común divisor de " + numero1 + " y " + numero2 + " es " + resultado);
+    }
+}
---
+++ src/main/java/org/example/Ejercicio5/MainIterativa.java
@@ -0,0 +1,8 @@
+package org.example.Ejercicio5;
+
+public class MainIterativa {
+    public static void main(String[] args) {
+        EuclidesIterativa mostrarMain = new EuclidesIterativa();
+        mostrarMain.main();
+    }
+}
